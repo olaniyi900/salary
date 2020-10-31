b260e39cdf3e95d1e303c3a97187a7eff3a60df5
Olaniyi Ajayi <ajayiolaniyi8@gmail.com>
2020-10-31 06:38:40 +0100

    modify html

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 
-let hourWork = document.querySelector('#hour-work');
-let extraWork = document.querySelector("#extra-hour");
-let calBtn = document.querySelector("#cal-btn");
-let payslip = document.querySelector("#payslip");
-let ts = document.querySelector("#ts");
-let tp = document.querySelector("#tp");
-let ep = document.querySelector("#ep");
+let hourWork = document.querySelector('#hour-work'); // target input field for main hour work
+let extraWork = document.querySelector("#extra-hour"); // target input fiield for extra hour work
+let calBtn = document.querySelector("#cal-btn"); // target submit button
+let payslip = document.querySelector("#payslip"); // target div for printing of pay slip
+let ts = document.querySelector("#ts"); // target li to display total salary
+let tp = document.querySelector("#tp"); // target li to display total main pay
+let ep = document.querySelector("#ep"); // target li to display extra pay
 
 
 // function to calculate salary
+// This function calculate total salary in hours plus extra over time pay
+// hourly pay is #1000 while extra pay is #200
+// Since the pay per hour is #1000 the extra hour work will be entitle to #1000 as well before calculating #200 on the extra hour.
+// the function take two argument one for main hour the other for extra hour.
 function salaryCalculator(hourlywork, extrahour) {
     if(typeof hourlywork === 'string' || typeof extrahour === 'string'){
         return 'Provide a number';
     }
-    let hourWage = 1000;
-    let extraWage = 200;
-    let totalHour = hourlywork + extrahour;
-    let totalPay = hourWage * totalHour;
-    let extraPay = extrahour * extraWage;
-    let totalSalary = totalPay + extraPay;
+    let hourWage = 1000; // pay per hour
+    let extraWage = 200; // overtime pay
+    let totalHour = hourlywork + extrahour; // total hour work both main and overtime 
+    let totalPay = hourWage * totalHour; // total pay excluding overtime
+    let extraPay = extrahour * extraWage; // overtime pay
+    let totalSalary = totalPay + extraPay; // salary
     return totalSalary;
 
 }
 
 
 // function to show pay slip
+// eventlistner to calculate.
 calBtn.addEventListener('click', function (e) {
     e.preventDefault();
 
-  // function to calculate salary
-    
 
     // display info
     let hw = Number(hourWork.value);
@@ -38,9 +41,9 @@ calBtn.addEventListener('click', function (e) {
     let totalPays = (hw + ew) * 1000
     let extraPays = ew *200
     console.log(total);
-    tp.innerText = `Total Hour Pay: ${totalPays}`;
-    ep.innerText = `Extra Pay: ${extraPays}`;
-    ts.innerText = `Total Salary: ${total}`;
+    tp.innerHTML = `Total Hour Pay: <span>&#8358;</span>${totalPays}`;
+    ep.innerHTML = `Extra Pay: <span>&#8358;</span>${extraPays}`;
+    ts.innerHTML = `Total Salary: <span>&#8358;</span>${total}`;
     hourWork.value = "";
     extraWork.value = "";
 });
